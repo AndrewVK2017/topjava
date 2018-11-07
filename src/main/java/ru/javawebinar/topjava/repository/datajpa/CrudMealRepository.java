f5adc9c5501ae2ae6d58de47738a045e31d936f1
Andrew <andrewvk2017@gmail.com>
2018-11-07 23:30:43 +0300

    5 9 jdk11 fix

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,25 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Transactional
+    int deleteByIdAndUserId(Integer id, Integer userId);
+
+    Optional<Meal> findByIdAndUserId(Integer id, Integer userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(Integer id);
+
+    List<Meal> findAllByDateTimeBetweenAndUserIdOrderByDateTimeDesc(LocalDateTime startDate, LocalDateTime endDate, Integer userId );
 }
